9f9287eeaccc68bc435cefe98c8028ce8a59a43b
Isaias Almeida <manoelisaiasa@gmail.com>
2019-09-15 13:03:08 -0300

    Add pacote Apollo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'chat-app';
+  private apiURL = "https://api.graph.cool/simple/v1/ck0k5hxsp3i8t0127meqvs7qr";
+
+  constructor(private http: HttpClient) {
+    this.createUser();
+    this.allUsers();
+  }
+
+  allUsers(): void {
+    const body = {
+      query: `
+        query {
+          allUsers {
+            id
+            name
+            email
+          }
+        }
+      `
+    };
+
+    this.http
+      .post(this.apiURL, body)
+      .subscribe(res => console.log("Query: ", res));
+  }
+
+  createUser(): void {
+    const body = {
+      query: `
+        mutation createNewUser($name: String!, $email: String!, $password: String!) {
+          createUser(name: $name, email: $email, password: $password) {
+            id
+            name
+            email
+          }
+        }
+      `,
+      variables: {
+        name: "Leninha Oliveira",
+        email: "leninha@gmail.com",
+        password: "123456"
+      }
+    };
+
+    this.http
+      .post(this.apiURL, body)
+      .subscribe(res => console.log("Mutation: ", res));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { MatToolbarModule, MatListModule } from "@angular/material";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [AppComponent],
@@ -12,6 +13,7 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
     // BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatListModule,
     MatToolbarModule
   ],
